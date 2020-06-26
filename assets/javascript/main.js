bcadfb17d6a7af6a8336d4fb175c35c42fec1fe6
Mark Nelson <mnelson400@comcast.net>
2020-06-26 15:20:30 -0500

    Notes W/REC Times

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,14 +3,14 @@
 let coin = {
     state: 0,
     flip: function() {
-        // this.state with Math.random here.
+        // this.state with Math.random here.(REC. 2020-06-26 13-26-37 @ 3:48)
     },
 
 
 /* 2. Return the string "Heads" or "Tails", depending on whether
     "this.state" is 0 or 1. */
     toString: function() {
-        // if/else here.
+        // if/else here.(REC. 2020-06-26 13-26-37 @ 9:47)
     },
 
 
@@ -18,7 +18,7 @@ let coin = {
     or face-down, depending on whether this.state is 0 or 1.*/
     toHTML: function() {
         let image = document.createElement('img');
-            // if/else image.srv here.
+            // if/else image.srv here.(REC. 2020-06-26 13-26-37 @ 9:47)
         return image;
     }
 };
@@ -28,7 +28,7 @@ let coin = {
 //  (make use of your toString() method)
 function display20Flips () {
     for (let index = 20; index < display20Flips.length; index += 1) {
-        // Return result of each flip.
+        // Return result of each flip.(REC. 2020-06-26 13-26-37 @ 11:42)
      }
 }
 
@@ -37,12 +37,16 @@ function display20Flips () {
 //  result of each flip as an HTML IMG element on the page (make use of your toHTML() method).
 function display20Images () {
     for (let index = 20; index < display20Images.length; index += 1) {
-        // Return image of the result of each flip. 
+        // Return image of the result of each flip.(REC. 2020-06-26 13-26-37 @ 11:42)
+        // assets/images/coin_heads.png - coin_tails.png
      }
 }
 
-// *Create a refresh button and append to HTML.
+// *Finished product should look something like this:(REC. 2020-06-26 13-26-37 @ 16:32)
 
+// *Create a refresh button and append to HTML.(Not needed, but Alec used one.)
+
+// *Check out math.round on W3Schools.(May help with the math.random.)
 
 // *Note that you will need to download or create images to use to display face-up or 
 //  face-down coins for the toHTML method. Create a folder directory named images in 
